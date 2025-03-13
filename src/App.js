c3701668cc186e2565dcb96022932b78d68caa5d
zainabyahya <zainabh_2015@yahoo.com>
2025-03-13 06:52:55 +0300

    update routing for learning modules

--- src/App.js
+++ src/App.js
@@ -7,22 +7,26 @@ import Community from "./pages/Community";
 import Learning from "./pages/Learning";
 import Library from "./pages/Library";
 import Profile from "./pages/Profile";
-
+import Footer from "./components/Footer";
+import LearningTopicPage from "./components/LearningTopicPage.jsx"
 function App() {
   return (
-    <div className=" w-full h-screen">
+    <div className="flex flex-col w-full min-h-screen">
       < Navbar />
-      <div className='min-h-[70vh]'>
+      <div className='flex-1'>
         <Routes>
           < Route path="/" element={<Home />} />
           < Route path="/assessment" element={<Assessment />} />
           < Route path="/community" element={<Community />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/learning" element={<Learning />} />
+          <Route path="/learning/*" element={<Learning />}>
+            <Route path=":areaId/:topic" element={<LearningTopicPage />} />
+          </Route>
           <Route path="/library" element={<Library />} />
           <Route path="/auth" element={<Auth />} />
         </Routes>
       </div>
+      <Footer />
     </div >
 
   );
