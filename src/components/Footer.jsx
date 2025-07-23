0e71c6750ae1cec7cf676f3fddd5ffa9ea6dd54f
zainabyahya <zainabh_2015@yahoo.com>
2025-07-23 01:53:31 +0100

    comment copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-primary text-white p-4 w-full" dir="rtl">
       <div className="container mx-auto text-center">
-        <p>© 2024 جميع الحقوق محفوظة لمنصة الكفاءة الرقمية</p>
+        {/* <p>© 2024 جميع الحقوق محفوظة لمنصة الكفاءة الرقمية</p> */}
         <div className="mt-2">
           <a href="/about" className="mx-2 hover:underline">
             حول
